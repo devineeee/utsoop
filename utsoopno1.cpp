3799d2a9e1b5afa8ce647b153e46fdc73e6f24f5
devineeee <aminadevine3@gmail.com>
2023-11-30 10:11:25 +0700

    first commit

---
+++ utsoopno1.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+    std::cout << "Uts oop no 1!";
+    
+
+    return 0;
+}
