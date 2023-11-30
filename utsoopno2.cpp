04a70036bc8587bdba741960ba643296b714ead6
devineeee <aminadevine3@gmail.com>
2023-11-30 10:20:00 +0700

    utsno2 oop

---
+++ utsoopno2.cpp
@@ -0,0 +1,12 @@
+// jawaban no2 bla bla....
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+    std::cout << "Uts oop no 1!";
+    
+
+    return 0;
+}
