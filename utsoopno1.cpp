1a0809176a8f6f69aaef48192cf9a3189ae3eb43
devineeee <aminadevine3@gmail.com>
2023-12-03 16:10:27 +0700

    utsoopno1

--- utsoopno1.cpp
+++ utsoopno1.cpp
@@ -1,11 +1,57 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 
+class PersegiPanjang {
+private:
+    double panjang;
+    double lebar;
+
+public:
+    // Setter untuk panjang
+    void setPanjang(double p) {
+        panjang = p;
+    }
+
+    // Getter untuk panjang
+    double getPanjang() const {
+        return panjang;
+    }
+
+    // Setter untuk lebar
+    void setLebar(double l) {
+        lebar = l;
+    }
+
+    // Getter untuk lebar
+    double getLebar() const {
+        return lebar;
+    }
+
+    // Method untuk menghitung luas
+    double hitungLuas() const {
+        return panjang * lebar;
+    }
+
+    // Method untuk menghitung keliling
+    double hitungKeliling() const {
+        return 2 * (panjang + lebar);
+    }
+};
+
 int main() {
-    // Write C++ code here
-    std::cout << "Hello world!";
-    std::cout << "Uts oop no 1!";
-    
+    // Membuat objek PersegiPanjang
+    PersegiPanjang persegiPanjang;
+
+    // Menggunakan setter untuk mengatur panjang dan lebar
+    persegiPanjang.setPanjang(12);
+    persegiPanjang.setLebar(2);
+
+    // Menggunakan getter untuk mendapatkan panjang dan lebar
+    std::cout << "Panjang: " << persegiPanjang.getPanjang() << std::endl;
+    std::cout << "Lebar: " << persegiPanjang.getLebar() << std::endl;
+
+    // Menghitung dan menampilkan luas dan keliling
+    std::cout << "Luas: " << persegiPanjang.hitungLuas() << std::endl;
+    std::cout << "Keliling: " << persegiPanjang.hitungKeliling() << std::endl;
 
     return 0;
 }
